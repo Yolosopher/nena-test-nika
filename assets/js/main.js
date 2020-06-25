a8d3e5a7290e56b2409aa9a00ca7a22b0897aeff
Yolosopher <61516311+Yolosopher@users.noreply.github.com>
2020-06-25 12:36:29 +0400

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,78 +13,80 @@ var swiper = new Swiper('.whole-slider-outer', {
 
 // gallery
 var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 0,
-    slidesPerView: 11,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    breakpoints: {
-        320: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        750: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1000: {
-          slidesPerView: 8,
-          spaceBetween: 0,
-        },
-      }
-  });
-  var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    thumbs: {
-      swiper: galleryThumbs,
+  spaceBetween: 0,
+  slidesPerView: 11,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    speed: 1500,
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    }
-  });
+    750: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1000: {
+      slidesPerView: 8,
+      spaceBetween: 0,
+    },
+  }
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  speed: 1500,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  }
+});
 
 
 
 
 
 $(document).ready(function () {
-    $('.each-ingr').mouseover(function() {
-      $(this).css('bottom', '30px');
-      $(this).next().css('bottom', '10px');
-      $(this).prev().css('bottom', '10px');
+  $('.each-ingr').mouseover(function () {
+    $(this).css('bottom', '30px');
+    $(this).next().css('bottom', '10px');
+    $(this).prev().css('bottom', '10px');
   })
-    $('.each-ingr').mouseleave(function() {
-      $(this).css('bottom', '0');
-      $(this).next().css('bottom', '0');
-      $(this).prev().css('bottom', '0');
-    })
+  $('.each-ingr').mouseleave(function () {
+    $(this).css('bottom', '0');
+    $(this).next().css('bottom', '0');
+    $(this).prev().css('bottom', '0');
+  })
 
 
-    $('.each-selecting-box').click(function() {
-      $('.each-selecting-box').removeClass('chosen');
-      $(this).addClass('chosen');
-      let src = $(this).find('img').attr('src');
-      
-      let mainChsnPhoto = $(this).parent().parent().parent().find('.main-chosen-photo img');
-      mainChsnPhoto.fadeOut(400, function() {
-        $(this).attr('src', src).fadeIn(400)
-      });
-      
-    })
+  $('.each-selecting-box').click(function () {
+    $('.each-selecting-box').removeClass('chosen');
+    $(this).addClass('chosen');
+    let src = $(this).find('img').attr('src');
 
-    // POPULAR PRODUCTS
-  //   $('.pp-each-box-li').mouseover(function() {
-  //     $(this).toggleClass('each-box-li-active');
-  //   });
-  // });
-  //   $('.pp-each-box-li').mouseleave(function() {
-  //     $(this).toggleClass('each-box-li-active');
-  //   });
-  // });
-  
+    let mainChsnPhoto = $(this).parent().parent().parent().find('.main-chosen-photo img');
+    mainChsnPhoto.fadeOut(400, function () {
+      $(this).attr('src', src).fadeIn(400)
+    });
+
+  })
+
+  // POPULAR PRODUCTS
+  $('.pp-each-box-li').mouseover(function () {
+    $(this).addClass('each-box-li-active');
+  });
+});
+$('.pp-each-box-li').mouseleave(function () {
+  $(this).removeClass('each-box-li-active');
+  $(this).addClass('each-box-li-active-remove');
+  setTimeout(() => {
+    $(this).removeClass('each-box-li-active-remove');
+  }, 900);
+});
