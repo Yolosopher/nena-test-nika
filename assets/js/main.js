fefb863308e7b47084de729decf0a16b74613e1d
Yolosopher <61516311+Yolosopher@users.noreply.github.com>
2020-06-25 15:40:59 +0400

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,28 +65,64 @@ $(document).ready(function () {
     $(this).prev().css('bottom', '0');
   })
 
+  // SELECTING BOXES
+  selectingBoxes('each-selecting-box', 'main-chosen-photo')
 
-  $('.each-selecting-box').click(function () {
-    $('.each-selecting-box').removeClass('chosen');
-    $(this).addClass('chosen');
-    let src = $(this).find('img').attr('src');
+  function selectingBoxes(each, main) {
+    $(`.${each}`).click(function () {
+      $(`.${each}`).removeClass('chosen');
+      $(this).addClass('chosen');
+      let src = $(this).find('img').attr('src');
+  
+      let mainChsnPhoto = $(this).parent().parent().parent().find(`.${main} img`);
+      mainChsnPhoto.fadeOut(400, function () {
+        $(this).attr('src', src).fadeIn(400)
+      });
+    })
+  }
 
-    let mainChsnPhoto = $(this).parent().parent().parent().find('.main-chosen-photo img');
-    mainChsnPhoto.fadeOut(400, function () {
-      $(this).attr('src', src).fadeIn(400)
-    });
-
-  })
 
   // POPULAR PRODUCTS
   $('.pp-each-box-li').mouseover(function () {
     $(this).addClass('each-box-li-active');
   });
-});
-$('.pp-each-box-li').mouseleave(function () {
-  $(this).removeClass('each-box-li-active');
-  $(this).addClass('each-box-li-active-remove');
-  setTimeout(() => {
-    $(this).removeClass('each-box-li-active-remove');
-  }, 900);
+  $('.pp-each-box-li').mouseleave(function () {
+    $(this).removeClass('each-box-li-active');
+    $(this).addClass('each-box-li-active-remove');
+    setTimeout(() => {
+      $(this).removeClass('each-box-li-active-remove');
+    }, 900);
+  });
+
+
+  // POPULAR PRODUCTS AUTOPLAY
+  let ppLis = document.querySelectorAll('.pp-each-box-li');
+  let currentLi = 0;
+  autoPlay();
+  setInterval(()=>{
+    autoPlay();
+  }, 6000);
+
+  function autoPlay() {
+    if ((currentLi-1) != -1) {
+      ppLis[currentLi-1].classList.remove('each-box-li-selected');
+    }
+    ppLis[currentLi].classList.add('each-box-li-selected');
+    currentLi++;
+    let a = currentLi;
+    if (a === ppLis.length) {
+      currentLi = 0;
+    };
+    srcChanger();
+  };
+
+  function srcChanger() {
+    if ($('.pp-each-box-li').hasClass('each-box-li-selected')) {
+      let src = $('.each-box-li-selected a img').attr('src');
+
+      $('.pp-main-box img').fadeOut(400, function() {
+        $(this).attr('src', src).fadeIn(400)
+      });
+    }
+  }
 });
